82629f659c2c00c903aac3fafb67102e94bec59d
samugax <samueleuzebiodeoliveira@hotmail.com>
2016-12-12 13:48:26 -0200

    Add files via upload

---
+++ BD1.sql
@@ -0,0 +1,18 @@
+﻿-- Table: public.usuario
+
+-- DROP TABLE public.usuario;
+
+CREATE TABLE public.usuario
+(
+  nome character varying NOT NULL,
+  email character varying NOT NULL,
+  senha character varying NOT NULL,
+  confsenha character varying NOT NULL,
+  foto character varying,
+  id integer NOT NULL DEFAULT nextval('usuario_id_seq'::regclass)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.usuario
+  OWNER TO postgres;
